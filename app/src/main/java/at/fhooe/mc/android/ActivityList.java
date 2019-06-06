01a1ef51735e8866d3bd91ba47fa02102663ee44
Ines Langbauer <S1810237022@students.fh-hagenberg.at>
2019-06-06 20:19:37 +0200

    button to Actionbar

--- app/src/main/java/at/fhooe/mc/android/ActivityList.java
+++ app/src/main/java/at/fhooe/mc/android/ActivityList.java
@@ -1,10 +1,13 @@
 package at.fhooe.mc.android;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,7 +16,7 @@ import com.google.firebase.auth.FirebaseAuth;
 /**
  * this class displays the toDoList and implements action that can manipulate the list
  */
-public class ActivityList extends Activity implements View.OnClickListener {
+public class ActivityList extends Activity  {
 
     private final static String TAG = "at.fhooe.mc.toDoList :: ActivityList";
 
@@ -24,43 +27,49 @@ public class ActivityList extends Activity implements View.OnClickListener {
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_list);
 
-        Button b = findViewById(R.id.list_Activity_LogOut_Button);
-        b.setOnClickListener(this);
+        final ActionBar ab = getActionBar();
+        ab.setHomeButtonEnabled(true);
+        //DataAdapter adapter = new DataAdapter(this);
 
-        b = findViewById(R.id.list_Activity_add_button);
-        b.setOnClickListener(this);
 
-        b = findViewById(R.id.list_Activity_delete_button);
-        b.setOnClickListener(this);
+        //for(long i = 1; 1<MainActivity.mTaskNumber;i++){}
 
+        // adapter.add(new ListData(Repository.getInstance().getStringData(ref)));
+        //setListAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu _menu) {
+        getMenuInflater().inflate(R.menu.menu_arlog, _menu);
+        return true;
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.list_Activity_LogOut_Button: {
+    public boolean onOptionsItemSelected(MenuItem _item) {
+        switch (_item.getItemId()){
+            case R.id.menu_arlog_logout:{
                 Log.i(TAG, "list_Activity::onClick logOut Button was pressed");
                 logOut();
-                finish();
-            }
-            break;
-            case R.id.list_Activity_add_button: {
+                finish();}break;
+            case R.id.menu_arlog_add: {
                 Log.i(TAG, "list_Activity::onClick add Button was pressed");
                 Intent i = new Intent(this, ActivityDeadlineTask.class);
                 long taskNumber = MainActivity.getTaskNumber() + 1;
                 MainActivity.setTaskNumber(taskNumber);
                 startActivity(i);
-            }
-            break;
-            case R.id.list_Activity_delete_button: {
+            }break;
+            case R.id.menu_arlog_remove: {
+                // long taskNumber = MainActivity.getTaskNumber() - 1;
+                //MainActivity.setTaskNumber(taskNumber);
                 Log.e(TAG, "list_Activity::onClick delete Button was pressed");
-
             }
             break;
             default:
                 Log.e(TAG, "list_Activity::onClick unexpected ID encountered");
-        }
+        }return true;
     }
+
 
     /**
      * logs the User out
